7318a747ed8c4232266ab392cc5e45a583f41fb1
Tayamul Rai <tayamul.rai@Tayamuls-Laptop.local>
2024-07-10 10:52:26 +0100

    add dependencies to build.sbt and create a test file to test TwoNumbers class

---
+++ src/main/scala/Week2/Wed/TwoNumbers.scala
@@ -0,0 +1,11 @@
+package Week2.Wed
+
+class TwoNumbers (x: Int, y: Int) {
+
+  def add(): Int = ??? // ??? == empty implementation
+
+  def subtract(): Int = ???
+
+  def multiply(): Int = ???
+
+}
---
+++ src/test/scala/Week2/Wed/TwoNumbersSpec.scala
@@ -0,0 +1,7 @@
+package Week2.Wed
+
+import org.scalatest.FlatSpec
+
+class TwoNumbersSpec extends FlatSpec {
+
+}
