9c00ee7954ed7752cee00eff660d7667527fc43a
Tayamul <rai_tayamul@hotmail.com>
2024-07-18 15:33:27 +0100

    define checkCreditScore method and implement tests to consider all possbile cases

--- src/main/scala/Week3/Thurs/MVP.scala
+++ src/main/scala/Week3/Thurs/MVP.scala
@@ -19,15 +19,31 @@ package Week3.Thurs
 
 object MVP extends App {
 
-  case class Person (name: String, income: Double, deposit: Double, priceOfFlat: Double, creditScore: Double)
-  val Samantha: Person = Person(name = "Samantha", income = 45000, deposit = 20000, priceOfFlat = 280000, creditScore = 577)
+  case class Person (name: String, income: Double, deposit: Double, priceOfHouse: Double, creditScore: Double)
+  val Samantha: Person = Person(name = "Samantha", income = 45000, deposit = 20000, priceOfHouse = 280000, creditScore = 577)
 
   def isEligibleForMortgage (person: Person): Either[String, String] = {
-    val mortgage: Double = person.priceOfFlat - person.deposit
+    val mortgage: Double = person.priceOfHouse - person.deposit
     val incomeX4: Double = person.income * 4
 
     if (incomeX4 < mortgage) Left(s"${person.name} is NOT eligible for mortgage.")
     else Right(s"${person.name} is eligible for mortgage.")
   }
 
+//  def acceptableDeposit (deposit: Double): Either[String, String] = {
+//
+//  }
+
+  def checkCreditScore (creditScore: Double): Either[String, Double] = {
+    // for edge cases - can the creditScore's value ever be 0 or a negative integer?
+    // what about the highest credit score value?
+
+    creditScore match {
+      case c if c < 0 => Left("Cannot input a negative integer.")
+      case 0 => Left("Input cannot be 0.")
+      case c if (c <= 999 && c >= 550) => Right(creditScore)
+      case c if c <= 550 => Left("The individual does not meet the minimum credit score requirement.")
+      case c if c > 999 => Left("The value is unrealistic.")
+    }
+  }
 }
--- src/test/scala/Week3/MVPSpec.scala
+++ src/test/scala/Week3/MVPSpec.scala
@@ -6,8 +6,8 @@ import org.scalatest.wordspec.AnyWordSpec
 
 class MVPSpec extends AnyWordSpec with Matchers {
 
-  val Dave: Person = Person(name = "Dave",  income = 80000, deposit = 20000, priceOfFlat = 280000, creditScore = 577)
-  val mortgage: Double = Dave.priceOfFlat - Dave.deposit
+  val Dave: Person = Person(name = "Dave",  income = 80000, deposit = 20000, priceOfHouse = 280000, creditScore = 577)
+  val mortgage: Double = Dave.priceOfHouse - Dave.deposit
   val incomeX4: Double = Dave.income * 4
 
   "isEligibleForMortgage" should {
@@ -19,6 +19,49 @@ class MVPSpec extends AnyWordSpec with Matchers {
     "return a Right" when {
       "the yearly income is more than four times the mortgage amount" in {
         isEligibleForMortgage(Dave) shouldBe Right(s"${Dave.name} is eligible for mortgage.")
+      }
+    }
+  }
+
+//  "acceptableDeposit" should {
+//    "return a Left" when {
+//      "the deposit amount is not 10% of the purchase price" in {
+//        acceptableDeposit(20000)
+//      }
+//    }
+//  }
+
+  "checkCreditScore" should {
+    "return a Right" when {
+      "the credit score is more than 550" in {
+        checkCreditScore(600) shouldBe Right(600)
+      }
+      "the credit score is exactly 550" in {
+        checkCreditScore(550) shouldBe Right(550)
+      }
+      "the credit score is at its highest possible value" in {
+        checkCreditScore(999) shouldBe Right(999)
+      }
+      "the credit score is just below its highest possible value" in {
+        checkCreditScore(998) shouldBe Right(998)
+      }
+    }
+
+    "return a Left" when {
+      "the credit score is below 550" in {
+        checkCreditScore(250) shouldBe Left("The individual does not meet the minimum credit score requirement.")
+      }
+      "the credit score is just below 550" in {
+        checkCreditScore(549) shouldBe Left("The individual does not meet the minimum credit score requirement.")
+      }
+      "the credit score is 0" in {
+        checkCreditScore(0) shouldBe Left("Input cannot be 0.")
+      }
+      "the input is a negative integer" in {
+        checkCreditScore(-300) shouldBe Left("Cannot input a negative integer.")
+      }
+      "the credit score is above the highest possible value" in {
+        checkCreditScore(1200) shouldBe Left("The value is unrealistic.")
       }
     }
   }
