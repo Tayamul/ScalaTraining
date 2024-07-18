bd1a27fe65e2b30fd1ed60bb7d6a953eb8d82247
Tayamul <rai_tayamul@hotmail.com>
2024-07-18 14:16:15 +0100

    create a case class Person and instantiate an object Samantha of this class

---
+++ src/main/scala/Week3/Thurs/MVP.scala
@@ -0,0 +1,8 @@
+package Week3.Thurs
+
+object MVP extends App {
+
+  case class Person (income: Double, deposit: Double, priceOfFlat: Double, creditScore: Double)
+  val Samantha: Person = Person(income = 45000, deposit = 20000, priceOfFlat = 280000, creditScore = 577)
+
+}
