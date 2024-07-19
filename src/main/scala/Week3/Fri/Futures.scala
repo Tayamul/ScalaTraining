fd15b41806dffc5f751a3297183efcf0151070ff
Tayamul <rai_tayamul@hotmail.com>
2024-07-19 18:30:49 +0100

    lesson on Futures

--- src/main/scala/Week3/Fri/Futures.scala
+++ src/main/scala/Week3/Fri/Futures.scala
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.{Duration, FiniteDuration}
+import scala.concurrent.impl.Promise
 
 object Futures extends App {
 
@@ -23,5 +24,32 @@ object Futures extends App {
 
   val waitTime: FiniteDuration = Duration(5, TimeUnit.SECONDS)
   println(Await.result(additionInTheFuture, waitTime)) // this will block the main thread.
+
+  class NewError (val errorCode: Int, val errorMessage: String) extends Exception
+  case object IsOddError extends NewError (400, "Bad request in the is odd method")
+
+  def isOdd (x: Int): Either[NewError, String] =
+    if (x % 2 != 0) Right(s"$x is odd") else Left(IsOddError)
+
+//  println(isOdd(20))
+//  println(isOdd(21))
+
+  /** Either implemented with Futures */
+  def fetchIsOddOrError (num: Int): Future[Either[NewError, String]] = Future {
+    Thread.sleep(5000)
+    isOdd(num)
+  }
+
+  def callFetchMethod: Future[Either[NewError, String]] = {
+    fetchIsOddOrError(21)
+  }
+
+  def patternMatchingFutureMethod: String = callFetchMethod.value match {
+    case Some(value) => s"Future method was a success: $value"
+    case None => "Future timed out!"
+  }
+
+  println(patternMatchingFutureMethod)
+//  println(Await.result(patternMatchingFutureMethod, waitTime))
 
 }
