3dfe2d55425ef9d91e6ab60854fe6f23673db2a2
Tayamul <rai_tayamul@hotmail.com>
2024-07-22 10:31:13 +0100

    Factorial example using  tail recursion

--- src/main/scala/week4/mon/Recursion.scala
+++ src/main/scala/week4/mon/Recursion.scala
@@ -1,5 +1,7 @@
 package week4.mon
 
+import scala.annotation.tailrec
+
 object Recursion extends App {
 
   // Standard recursion
@@ -19,4 +21,33 @@ object Recursion extends App {
   println(factorial(4))
 //  println(factorial(400000000)) // stack overflow error - limited memory on the call stack
 
+  /** Tail recursion
+   * Helps stop the stack overflow error - by reusing the call stack rather than just adding to it.
+   * Looks trickier because there's method within a method -
+   * THE helper function reuses the method - the main method does not need to be used or worked on
+   * "Helper function" - write all the method inside here, and only uses the main method for the 'last' call.
+   * You can have multiple helper methods, or multiple accumulators
+   * acc is going to store the data for us, rather than the call stack
+   * */
+  def smartFactorial (n: Int): Int = { // user doesn't need to think about passing in accumulator, everything is done with the helper function
+    @tailrec
+    def factorialHelper(x: Int, acc: Int): Int = { // helper method (has an accumulator + any additional params that may be needed during the recursion, but not in the original method)
+      if (x <= 1) acc
+      else {
+        //        println(s"Current X: $x")
+        //        println(s"Current acc: $acc")
+        factorialHelper(x - 1, x * acc) // acc - holds the running total - storing that intermediate
+      }
+    }
+    factorialHelper(n, 1)
+  }
+  println("\nFactorial (4) - tail rec:")
+  println(smartFactorial(4))
+
+  /** 1st call: factorialHelper(4-1, 4*1) = (3, 4)
+   * 2nd call: factorialHelper(3-1, 3*(4*1)) = (2, 12)
+   * 3rd call: factorialHelper(2, 2 * (3*(4*1))) = (1, 24)
+   * 4th call: x == 1 ==> hitting the 'if' and returning the acc which is currently stored at 24.
+   */
+  println(smartFactorial(400000))
 }
